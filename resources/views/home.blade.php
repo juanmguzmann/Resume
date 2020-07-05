5127e2b9487b0c056c9b8550eaefc916558b7bb6
Juan Guzman <juanguzman@MacBook-Pro-de-Juan.local>
2020-07-05 19:00:12 -0300

    Prueba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
     <script src="https://kit.fontawesome.com/a340b68414.js" crossorigin="anonymous"></script>
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
     <link rel="stylesheet" href="css/styles.css">
-    <title>Juan Marco Guzman</title>
+    <title>Juan Marcos Guzman</title>
   </head>
   <body>
     <div class="container-fluid">
